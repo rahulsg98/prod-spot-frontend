92d6af171a30bc276cf1d2302b36cc6195981ac8
rahulsg98 <rahulsinghgurjar10000@gmail.com>
2023-06-25 15:48:33 +0530

    IntroSection changes

--- src/Components/Main/IntroSection/IntroSection.jsx
+++ src/Components/Main/IntroSection/IntroSection.jsx
@@ -9,7 +9,7 @@ const IntroSection = () => {
       <div className='intro-section-text'>
         <p className='intro-section-text-1'>Add your products and give <br /> your valuable feedback </p>
         <p className='intro-section-text-2'>
-          Easily give your feedback in a matter of minutes. Access your <br />audience on all platforms. Observe result manually in real time
+          Easily give your feedback in a matter of minutes. Access your audience on all platforms. Observe result manually in real time
         </p>
       </div>
     </div>
